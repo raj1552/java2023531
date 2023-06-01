2ae9feff6b1e82b7665d1b04112fbca37e8c401e
raj1552 <anurajshrestha75@gmail.com>
2023-06-01 17:01:13 +0545

    merocommit

--- index.java
+++ index.java
@@ -13,56 +13,52 @@ class index {
         return a / b;
     }
 
-    public static void main(String args[]) {
-        int x, y;
-        try (Scanner scan = new Scanner(System.in)) {
-            while (true) {
-                System.out.println("Enter a number");
-                String input = scan.nextLine();
+    static int checkvalidinteger(Scanner scan, String message) {
+        int number;
+        while (true) {
+            System.out.print(message);
+            String input = scan.nextLine();
 
-                try {
-                    x = Integer.parseInt(input);
-                    break;
-                } catch (NumberFormatException e) {
-                    System.out.println("Invalid input!!");
-                }
-            }
-            while (true) {
-                System.out.println("Enter your second number");
-                String input = scan.nextLine();
-            
             try {
-                y = Integer.parseInt(input);
+                number = Integer.parseInt(input);
                 break;
             } catch (NumberFormatException e) {
                 System.out.println("Invalid input!!");
             }
-   }
+        }
+        return number;
+    }
+
+    public static void main(String args[]) {
+        int x, y;
+        try (Scanner scan = new Scanner(System.in)) {
+            x = checkvalidinteger(scan, "Enter first number");
+            y = checkvalidinteger(scan, "Enter Second number");
         }
 
         System.out.println("Press 1 for addition");
         System.out.println("Press 2 for Subtraction");
         System.out.println("Press 3 for Division");
 
-        try (Scanner ne = new Scanner(System.in)) {
-        int z=ne.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            int z = sc.nextInt();
 
-        if(z==1){
-        int result=add(x,y);
-        System.out.println("The sum is "+ result);
-        }
+            if (z == 1) {
+                int result = add(x, y);
+                System.out.println("The sum is " + result);
+            }
 
-        else if(z==2){
-        int result=sub(x, y);
-        System.out.println("The difference is "+result);
-        }
-        else if(z==3){
-        int result=div(x,y);
-        System.out.println("The division is "+result);
-        }
-        else {
-        System.out.println("Invalid");
-        }
+            else if (z == 2) {
+                int result = sub(x, y);
+                System.out.println("The difference is " + result);
+            }
+             else if (z == 3) {
+                int result = div(x, y);
+                System.out.println("The division is " + result);
+            } 
+            else {
+                System.out.println("Invalid");
+            }
         }
 
     }
